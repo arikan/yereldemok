e1577cbb31cc0bc9c1e5db562eaaffe63494448d
arikan <arikan@gmail.com>
2014-09-22 21:47:49 -0400

    eutr flag added

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <a href="http://hyd.org.tr">Helsinki Yurttaşlar Derneği</a>
+    <div class="pull-left">
+      <a href="http://hyd.org.tr" target="_blank">Helsinki Yurttaşlar Derneği</a>
+    </div>
+    <div class="pull-right">
+      <img src="wp-content/themes/roots/assets/img/eutr.png" class="img-responsive" width="100">
+    </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
