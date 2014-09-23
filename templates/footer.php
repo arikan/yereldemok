a910e141707fe52f088419e416639d023a1be94b
arikan <arikan@gmail.com>
2014-09-22 22:10:53 -0400

    footer grid refind

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,9 @@
 <footer class="content-info" role="contentinfo">
   <div class="container raw">
-    <div class="pull-left .col-xs-12 .col-sm-6 .col-lg-8">
+    <div class="pull-left col-xs-6">
       <a href="http://hyd.org.tr" target="_blank">Helsinki Yurttaşlar Derneği</a>
     </div>
-    <div class="pull-right .col-xs-6 .col-lg-4">
+    <div class="pull-right col-xs-6">
       <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive" width="100">
     </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
