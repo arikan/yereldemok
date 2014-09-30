2330484aae8317c4c18ecfbbb024c2f4e51e70b8
arikan <arikan@gmail.com>
2014-09-30 13:58:33 -0400

    carousel indicators posistion fixed

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
       <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
       <a href="http://hyd.org.tr" target="_blank">Helsinki <br/>Yurttaşlar<br/> Derneği</a>
     </div>
-    <div class="pull-right col-xs-2">
+    <div class="pull-right col-xs-4">
       <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive pull-right" width="100">
     </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
