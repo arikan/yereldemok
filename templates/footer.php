b94df31468da65e075e7b0ece503c0e576629f46
arikan <arikan@gmail.com>
2014-09-30 13:03:50 -0400

    footer refined

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,8 @@
 <footer class="content-info" role="contentinfo">
   <div class="container raw">
     <div class="pull-left col-xs-6">
-      <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive" width="100">
-      <a href="http://hyd.org.tr" target="_blank">Helsinki Yurttaşlar Derneği</a>
+      <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
+      <a href="http://hyd.org.tr" target="_blank">Helsinki <br/>Yurttaşlar<br/> Derneği</a>
     </div>
     <div class="pull-right col-xs-2">
       <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive" width="100">
