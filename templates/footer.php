2a64e8590bce0e8c119464becef65c1578a6671f
arikan <arikan@gmail.com>
2014-09-30 12:57:06 -0400

    hyd logo added

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,10 @@
 <footer class="content-info" role="contentinfo">
   <div class="container raw">
     <div class="pull-left col-xs-6">
+      <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hyd.png" class="img-responsive" width="100">
       <a href="http://hyd.org.tr" target="_blank">Helsinki Yurttaşlar Derneği</a>
     </div>
-    <div class="pull-right col-xs-6">
+    <div class="pull-right col-xs-2">
       <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive" width="100">
     </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
