cea932acb97b13f15ddd15bc94b2e726e98a3ccc
arikan <arikan@gmail.com>
2015-04-05 22:09:57 -0400

    footer updated

--- templates/footer.php
+++ templates/footer.php
@@ -1,14 +1,24 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container raw">
-    <div class="pull-left col-xs-6">
-      <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
-      <a href="http://hyd.org.tr" target="_blank">Helsinki <br/>Yurttaşlar<br/> Derneği</a>
+    <div class="container raw">
+        <div class="col-xs-4">
+            <a href="http://hyd.org.tr" target="_blank">
+                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
+                Helsinki <br/>Yurttaşlar<br/> Derneği
+            </a>
+        </div>
+        <div class="col-xs-4">
+
+        </div>
+        <div class="col-xs-4">
+            <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive pull-right" width="100">
+        </div>
+        <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
-    <div class="pull-right col-xs-4">
-      <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive pull-right" width="100">
+    <div class="container raw">
+        <div class="col-xs-12 disclaimer">
+            Bu web sitesi, KAGED tarafından yürütülen ve Avrupa Birliği ve Türkiye Cumhuriyeti tarafından ortaklaşa tarafından desteklenen “İnsan Haklarının Korunması ve Geliştirilmesine Yönelik Karar Alma Süreçlerinde Diyalogun Arttırılması İçin STK Ağlarının Güçlendirilmesi” projesi kapsamında yayın yapmaktadır. Web sitesinin Avrupa Birliği'nin resmi görüşlerini yansıttığı düşünülmemelidir.
+        </div>
     </div>
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
 </footer>
 
 <?php wp_footer(); ?>
