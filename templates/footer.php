fb9a387fe942a20d9ce00d2d6f543e3d11637307
arikan <arikan@gmail.com>
2015-04-05 23:12:05 -0400

    iletisim added to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,11 @@
 <footer class="content-info" role="contentinfo">
     <div class="container raw">
         <div class="col-xs-4">
-            <a href="http://www.cfcu.gov.tr/" target="_blank">
+            <a href="http://www.cfcu.gov.tr/" target="_blank" class="pull-left">
                 <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/cfcu.png" class="img-responsive">
+            </a>
+            <a href="http://www.cfcu.gov.tr/" target="_blank">
+                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/abb.png">
             </a>
         </div>
         <div class="col-xs-4 text-center">
@@ -12,12 +15,16 @@
             </a>
         </div>
         <div class="col-xs-4 text-right">
-            <a href="http://www.cfcu.gov.tr/" target="_blank">
-                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/abb.png">
-            </a>
+            <address>
+                <strong>İletişim:</strong><br>
+                Dr. Refik Saydam Cad. No: 167 (39), D:10<br>
+                Sishane 34430 Beyoglu, Istanbul<br>
+                +90 212 292 68 42 – 43<br>
+                <a href="mailto:info@hyd.org.tr">info@hyd.org.tr</a>
+            </address>
         </div>
-        <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
+
     <div class="container raw">
         <div class="col-xs-12 disclaimer">
             Bu web sitesi, KAGED tarafından yürütülen ve Avrupa Birliği ve Türkiye Cumhuriyeti tarafından ortaklaşa tarafından desteklenen “İnsan Haklarının Korunması ve Geliştirilmesine Yönelik Karar Alma Süreçlerinde Diyalogun Arttırılması İçin STK Ağlarının Güçlendirilmesi” projesi kapsamında yayın yapmaktadır. Web sitesinin Avrupa Birliği'nin resmi görüşlerini yansıttığı düşünülmemelidir.
