36baa51a93d01c3f00b79890f4c6d679a1118c8a
arikan <arikan@gmail.com>
2015-04-05 22:26:40 -0400

    footer updated

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,20 @@
 <footer class="content-info" role="contentinfo">
     <div class="container raw">
         <div class="col-xs-4">
-            <a href="http://hyd.org.tr" target="_blank">
-                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
-                Helsinki <br/>Yurttaşlar<br/> Derneği
+            <a href="http://www.cfcu.gov.tr/" target="_blank">
+                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/cfcu.png" class="img-responsive">
             </a>
         </div>
-        <div class="col-xs-4">
-
+        <div class="col-xs-4 text-center">
+            <a href="http://hyd.org.tr" target="_blank" class="hyd-logo">
+                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/hYd.png" class="img-responsive pull-left" width="75">
+                <span class="hyd-logo-txt">Helsinki <br/>Yurttaşlar<br/> Derneği</span>
+            </a>
         </div>
-        <div class="col-xs-4">
-            <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/eutr.png" class="img-responsive pull-right" width="100">
+        <div class="col-xs-4 text-right">
+            <a href="http://www.cfcu.gov.tr/" target="_blank">
+                <img src="http://yereldemokrasi.net/wp-content/themes/roots/assets/img/abb.png">
+            </a>
         </div>
         <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
