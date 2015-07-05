733b782f09af702661a3184341ae68f2516d6dfb
arikan <arikan@gmail.com>
2015-07-05 15:13:07 +0300

    search result removed from error pages

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
+  <!-- <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'roots'); ?>
-  </div>
-  <?php get_search_form(); ?>
+  </div> -->
 <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
