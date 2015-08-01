dd48ad449107642a497e65eb961f111d9b09e617
arikan <arikan@gmail.com>
2015-08-01 17:15:25 +0300

    media buttons code added as template

---
+++ templates/article-media.php
@@ -0,0 +1,30 @@
+<div class="row media-buttons">
+    <div class="col-md-4">
+        <a href="#video" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-film"></span> Toplantı Videosu</a>
+    </div>
+    <div class="col-md-4">
+        <a href="#infografik" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-signal"></span> Bölge İnfografik</a>
+    </div>
+    <div class="col-md-4">
+        <a href="#harita" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-tree-conifer"></span> Vakıa Ağ Haritası</a>
+    </div>
+</div>
+
+
+<a name="video"></a>
+<hr />
+<h2>Toplantı Videosu</h2>
+Bu toplantının videosu düzenlendiğinde eklenecek.
+<a href="https://www.youtube.com/playlist?list=PLJrKltA4FVX1lQZRcVXSQNOENWQB5HOG8" target="_blank">Diğer toplantı videoları için buraya tıklayınız</a>
+
+
+<a name="infografik"></a>
+<hr />
+<h2>Bölge Hakkında İnfografik</h2>
+Toplantı araştırması ile ilgili infografik çalışması sunuma hazır olduğunda eklenecek.
+
+
+<a name="harita"></a>
+<hr />
+<h2>Vakıa Ağ Haritası</h2>
+Bu bölge için vakıa ağ haritası hazır olduğunda eklenecek.
